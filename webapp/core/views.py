cd61cad983e9dc16ae43322f362cd44fe27831ff
Chen Liang <vagarychen@gmail.com>
2020-12-13 11:59:10 -0800

    Add image view on the home page (Caution! imges downloaded from internet)

--- webapp/core/views.py
+++ webapp/core/views.py
@@ -10,10 +10,10 @@ def index():
     This is the home page view
     '''
     places = {}
-    places["locationA"] = "A great place for hiking!!"
-    places["locationB"] = "A somewhat good place for hiking!"
-    places["locationC"] = "A not-so good place for hiking"
-    places["locationD"] = "A bad good place for hiking.."
+    places["locationA"] = ["A great place for hiking!!", "image1"]
+    places["locationB"] = ["A somewhat good place for hiking!", "image2"]
+    places["locationC"] = ["A not-so good place for hiking", "image3"]
+    places["locationD"] = ["A not-so good place for hiking", "image4"]
     return render_template('index.html', places=places)
 
 
